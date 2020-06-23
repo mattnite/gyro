e80b36dbe66222931a1f7a251d1d0c5cd9740a9a
Matt Knight <mattnite@protonmail.com>
2020-06-23 00:10:23 -0700

    added dependency graph and printing

--- src/main.zig
+++ src/main.zig
@@ -1,77 +1,167 @@
-usingnamespace std.zig;
 const std = @import("std");
-const zag = @import("zag.zig");
 const os = std.os;
-const c = @cImport({
-    @cInclude("git2.h");
-});
 
 const Allocator = std.mem.Allocator;
-const Pkg = std.build.Pkg;
+const ArrayList = std.ArrayList;
+const TailQueue = std.TailQueue;
+const OutStream = std.fs.File.OutStream;
 
-pub fn main() anyerror!void {
-    if (c.git_libgit2_init() == 0) {
-        defer _ = c.git_libgit2_shutdown();
+const DependencyGraph = struct {
+    allocator: *Allocator,
+    nodes: TailQueue(Node),
+    queue: TailQueue(Node),
+
+    const Self = @This();
+
+    /// Create DependencyGraph given a root path
+    fn init(allocator: *Allocator, path: []const u8) !DependencyGraph {
+        var ret = DependencyGraph{
+            .allocator = allocator,
+            .nodes = TailQueue.init(),
+            .queue = TailQueue.init(),
+        };
+
+        // TODO: get cwd of process
+        ret.queue.append(try ret.queue.createNode(Node.init(allocator, ".", "", 0), allocator));
     }
 
-    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    defer arena.deinit();
+    fn process(self: *Self) !void {
+        var maybe_front = self.queue.popFirst();
+        iterate: while (maybe_front) : (maybe_front = self.queue.popFirst()) {
+            const front = maybe_front.?.data;
 
-    const allocator = &arena.allocator;
-    const root_dir = std.fs.Dir{ .fd = try os.open(".", 0, 0) };
-    defer os.close(root_dir.fd);
+            // destroy if it already exists
+            var cursor = self.nodes.first;
+            while (cursor != null) : (cursor = cursor.next) {
+                const node = cursor.?.data;
+                if (std.mem.eql(u8, front.base_path, node.base_path) and
+                    std.mem.eql(u8, front.version, node.version))
+                {
+                    self.queue.destroyNode(maybe_front.?, self.allocator);
+                    continue :iterate;
+                }
+            }
 
-    const buf = try root_dir.readFileAlloc(allocator, "imports.zig", 1000);
-    defer allocator.free(buf);
+            // compile pkg_runner
 
-    const tree = try parse(allocator, buf);
-    defer tree.deinit();
+            // TODO: figure out some sort of system for keeping a giant buffer
+            // of all the output of the pkg_runners where the strings can live,
+            // and figure out how to do line by line iteration
+            var lines = try front.fetch_dependencies();
+            for (lines) |line| {
+                // if not in nodes
+                // set up edges
+                // set depth to current + 1
 
-    // we care about public vardecls that are const and have an init_node
-    // TODO: will have to remove requirement of init_node to be a call
-    var decls = std.ArrayList(*const ast.Node.VarDecl).init(allocator);
-    for (tree.root_node.declsConst()) |decl| {
-        if (decl.id != .VarDecl)
-            continue;
+                // else if it is in nodes and its depth is less than front
+                // increase depth of node (in node list) to depth + 1
+                // destroy dep
 
-        const vardecl = @fieldParentPtr(ast.Node.VarDecl, "base", decl);
-        if (vardecl.init_node) |init| {
-            if (init.id != .Call)
-                continue;
+                // append to queue
+            }
 
-            if (vardecl.visib_token) |idx| {
-                if (tree.token_ids[idx] == .Keyword_pub) {
-                    try decls.append(vardecl);
+            try self.validate();
+        }
+    }
+
+    /// Naive check for circular dependencies
+    fn validate(self: *Self) !void {
+        var cursor = self.nodes.first;
+        while (cursor != null) : (cursor = cursor.next) {
+            const node = cursor.?.data;
+
+            for (node.dependencies.span()) |dep| {
+                if (dep.node.depth <= node.depth) {
+                    return error.CyclicalDependency;
+                }
+            }
+
+            for (node.dependents.span()) |dep| {
+                if (dep.node.depth >= node.depth) {
+                    return error.CyclicalDependency;
                 }
             }
         }
     }
 
-    // check variable decl initializations to check if they are zag function
-    // calls
-    // TODO: Later this would check if the type returned is Import, and allow
-    // users to fill in the Import interface for customization. Requires zig
-    // interpreter.
-    var imports = std.ArrayList(zag.import.Import).init(allocator);
-    defer imports.deinit();
+    const DependencyEdge = struct {
+        node: *Node,
+        alias: []const u8,
+        root: []const u8,
+    };
 
-    for (decls.span()) |decl| {
-        const call = @fieldParentPtr(ast.Node.Call, "base", decl.init_node.?);
-        const params = call.paramsConst();
-        if (params.len != 3) {
-            return error.InvalidParams;
+    const DependentEdge = struct {
+        node: *Node,
+    };
+
+    const Node = struct {
+        dependencies: ArrayList(DependencyEdge),
+        dependents: ArrayList(DependentEdge),
+
+        base_path: []const u8,
+        version: []const u8,
+        depth: u32,
+
+        fn init(allocator: *Allocator, base_path: []const u8, version: []const u8, depth: u32) !Node {
+            return Node{
+                .dependencies = ArrayList.init(),
+                .dependents = ArrayList(),
+                .base_path = base_path,
+                .version = version,
+                .depth = depth,
+            };
         }
 
-        const alias = tree.tokenSlice(decl.name_token);
-        const repo = try literal_to_string(allocator, tree, params[0]);
-        const branch = try literal_to_string(allocator, tree, params[1]);
-        var root = if (params[2].id == .NullLiteral)
-            null
-        else
-            try literal_to_string(allocator, tree, params[2]);
+        fn compile_runner() !Runner {
+            var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+            defer arena.deinit();
 
-        try imports.append(zag.import.git_alias(alias, repo, branch, root));
-    }
+            const allocator = &arena.allocator;
+
+            var build_buf: [std.os.PATH_MAX]u8 = undefined;
+            const build_dir = try std.os.getcwd(&build_buf);
+            const cache_dir = try std.fs.path.join(allocator, &[_][]const u8{ build_dir, "zig-cache" });
+
+            std.debug.warn("build dir: {}\n", .{build_dir});
+            std.debug.warn("cache dir: {}\n", .{cache_dir});
+
+            const builder = try Builder.create(allocator, "zig", build_dir, cache_dir);
+            defer builder.destroy();
+
+            builder.resolveInstallPrefix();
+
+            // normal build script
+            const target = b.standardTargetOptions(.{});
+            const mode = b.standardReleaseOptions();
+
+            const exe = b.addExecutable("pkg_runner", "src/pkg_runner.zig");
+            exe.setTarget(target);
+            exe.setBuildMode(mode);
+            exe.install();
+
+            // TODO: this should just invoke the default step and build the program
+            try b.make(&[_][]const u8{});
+
+            return Runner{};
+        }
+    };
+};
+
+pub fn main() anyerror!void {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+
+    const allocator = &arena.allocator;
+
+    var dep_graph = try DependencyGraph.init(allocator, ".");
+    defer dep_graph.deinit();
+
+    // steps of the program:
+    // - create root of dependency graph
+    // - compile and run pkg_runner
+    // - read stdout of pkg_runner and create package objects, add package
+    //   objects to graph
+    try dep_graph.process();
 
     // generate package file
     var cache_dir = std.fs.Dir{ .fd = try os.open("zig-cache", os.O_DIRECTORY, 0) };
@@ -92,39 +182,37 @@ pub fn main() anyerror!void {
         \\
     );
 
-    for (imports.span()) |import| {
-        const import_path = try import.path(&import, allocator);
-        defer allocator.free(import_path);
-
-        const root_path = try std.mem.join(allocator, std.fs.path.sep_str, &[_][]const u8{ import_path, import.root });
-        defer allocator.free(root_path);
-
-        try import.fetch(&import, allocator);
-        try file_stream.print(
-            \\    Pkg{{
-            \\        .name = "{}",
-            \\        .path = "{}",
-            \\        .dependencies = null,
-            \\    }},
-            \\
-        , .{ import.alias, root_path });
+    for (node.dependencies.span()) |dep| {
+        try recursive_print(file_stream, dep, 1);
     }
 
     try file_stream.writeAll("};\n");
 }
 
-fn literal_to_string(allocator: *Allocator, tree: *ast.Tree, param: *ast.Node) ![]const u8 {
-    const literal = tree.tokenSlice(@fieldParentPtr(ast.Node.StringLiteral, "base", param).token);
-    const str = try remove_quotes(literal);
-    return str;
+fn indent(stream: OutStream, n: usize) !void {
+    try stream.writeByteNTimes(' ', n * 4);
 }
 
-fn remove_quotes(str: []const u8) ![]const u8 {
-    if (str.len < 2)
-        return error.TooShort;
+fn recursive_print(stream: std.fs.File.OutStream, edge: *DependencyEdge, depth: usize) !void {
+    try indent(stream, depth);
+    try stream.print("Pkg{{]\n", .{});
+    try indent(stream, depth + 1);
+    try stream.print(".name = \"{}\",\n", .{});
+    try indent(stream, depth + 1);
+    try stream.print(".path = \"{}\",\n", .{});
 
-    if (str[0] != '"' or str[str.len - 1] != '"')
-        return error.MissingQuotes;
+    if (node.dependencies.len) {
+        try indent(stream, depth + 1);
+        try stream.print(".dependencies = &[_]Pkg{{\n", .{});
 
-    return str[1 .. str.len - 1];
+        for (node.dependencies.span()) |dep| {
+            try recursive_print(stream, dep, depth + 2);
+        }
+    } else {
+        try indent(stream, depth + 1);
+        try stream.print(".dependencies = null,\n", .{});
+    }
+
+    try indent(stream, depth + 1);
+    try stream.print(".path = \"{}\",\n", .{});
 }
