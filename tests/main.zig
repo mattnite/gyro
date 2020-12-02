52aecac236268417647c59f3c8fe84c5b19dd47c
Matthew Knight <mgk1795@gmail.com>
2020-12-02 08:52:05 -0800

    Get this thing working on windows (#22)
    * added unit tests to windows ci
    
    * fixed paths in tests
    
    * playing with env vars
    
    * actually building after fetching in tests
    
    * added path fix
    
    * Update windows.yml
    
    * fixing path
    
    * need to add path to tests
    
    * added pwd path stuff
    
    * added proper windows pathing
    
    * added .exe
    
    * added linux pathing
    
    * back now
    
    * trying to use bash on windows

--- tests/main.zig
+++ tests/main.zig
@@ -3,8 +3,11 @@ const testing = std.testing;
 const ChildProcess = std.ChildProcess;
 const Term = ChildProcess.Term;
 
-fn zkgFetch(cwd: []const u8) !ChildProcess.ExecResult {
+fn zkgFetch(components: []const []const u8) !ChildProcess.ExecResult {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const cwd = try std.fs.path.join(&gpa.allocator, components);
+    defer gpa.allocator.free(cwd);
+
     return ChildProcess.exec(.{
         .allocator = &gpa.allocator,
         .argv = &[_][]const u8{ "zkg", "fetch" },
@@ -12,27 +15,43 @@ fn zkgFetch(cwd: []const u8) !ChildProcess.ExecResult {
     });
 }
 
+fn zigBuild(components: []const []const u8) !ChildProcess.ExecResult {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const cwd = try std.fs.path.join(&gpa.allocator, components);
+    defer gpa.allocator.free(cwd);
+
+    return ChildProcess.exec(.{
+        .allocator = &gpa.allocator,
+        .argv = &[_][]const u8{ "zig", "build" },
+        .cwd = cwd,
+    });
+}
+
 test "example" {
-    const result = try zkgFetch("tests/example");
+    const dir = [_][]const u8{ "tests", "example" };
+    var result = try zkgFetch(&dir);
+    testing.expectEqual(Term{ .Exited = 0 }, result.term);
+
+    result = try zigBuild(&dir);
     testing.expectEqual(Term{ .Exited = 0 }, result.term);
 }
 
 test "diamond" {
-    const result = try zkgFetch("tests/diamond");
+    const result = try zkgFetch(&[_][]const u8{ "tests", "diamond" });
     testing.expectEqual(Term{ .Exited = 0 }, result.term);
 }
 
 test "circular" {
-    const result = try zkgFetch("tests/circular");
+    const result = try zkgFetch(&[_][]const u8{ "tests", "circular" });
     testing.expectEqual(Term{ .Exited = 1 }, result.term);
 }
 
 test "self-centered" {
-    const result = try zkgFetch("tests/self-centered");
+    const result = try zkgFetch(&[_][]const u8{ "tests", "self-centered" });
     testing.expectEqual(Term{ .Exited = 1 }, result.term);
 }
 
 test "integrity" {
-    const result = try zkgFetch("tests/integrity");
+    const result = try zkgFetch(&[_][]const u8{ "tests", "integrity" });
     testing.expectEqual(Term{ .Exited = 0 }, result.term);
 }
