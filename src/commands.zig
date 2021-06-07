7f47098bdcc6d18f01adad5a2f96175394f8d3d8
Matt Knight <mattnite@protonmail.com>
2021-06-06 20:18:31 -0700

    fix args, add redirect file logic

--- src/commands.zig
+++ src/commands.zig
@@ -779,3 +779,117 @@ pub fn publish(allocator: *Allocator, pkg: ?[]const u8) !void {
 
     try api.postPublish(allocator, access_token.?, project.get(name).?);
 }
+
+fn validateDepsAliases(redirected_deps: []const Dependency, project_deps: []const Dependency) !void {
+    for (redirected_deps) |redirected_dep| {
+        for (project_deps) |project_dep| {
+            if (std.mem.eql(u8, redirected_dep.alias, project_dep.alias)) break;
+        } else {
+            std.log.err("'{s}' redirect does not exist in project dependencies", .{redirected_dep.alias});
+            return error.Explained;
+        }
+    }
+}
+
+fn moveDeps(redirected_deps: []const Dependency, project_deps: []Dependency) !void {
+    for (redirected_deps) |redirected_dep| {
+        for (project_deps) |*project_dep| {
+            if (std.mem.eql(u8, redirected_dep.alias, project_dep.alias)) {
+                project_dep.* = redirected_dep;
+                break;
+            }
+        } else unreachable;
+    }
+}
+
+pub fn redirect(
+    allocator: *Allocator,
+    clean: bool,
+    build_dep: bool,
+    alias_opt: ?[]const u8,
+    path_opt: ?[]const u8,
+) !void {
+    var arena = std.heap.ArenaAllocator.init(allocator);
+    defer arena.deinit();
+
+    const project_file = try std.fs.cwd().openFile("gyro.zzz", .{
+        .read = true,
+        .write = true,
+    });
+    defer project_file.close();
+
+    var gyro_dir = try std.fs.cwd().makeOpenPath(".gyro", .{});
+    defer gyro_dir.close();
+
+    const redirect_file = try gyro_dir.createFile("redirects", .{
+        .truncate = false,
+        .read = true,
+    });
+    defer redirect_file.close();
+
+    var project = try Project.fromFile(allocator, project_file);
+    defer project.destroy();
+
+    var redirects = try Project.fromFile(allocator, redirect_file);
+    defer redirects.destroy();
+
+    if (clean) {
+        try validateDepsAliases(redirects.deps.items, project.deps.items);
+        try validateDepsAliases(redirects.build_deps.items, project.build_deps.items);
+
+        try moveDeps(redirects.deps.items, project.deps.items);
+        try moveDeps(redirects.build_deps.items, project.build_deps.items);
+
+        redirects.deps.clearRetainingCapacity();
+        redirects.build_deps.clearRetainingCapacity();
+    } else {
+        const alias = alias_opt orelse {
+            std.log.err("missing alias argument", .{});
+            return error.Explained;
+        };
+
+        const path = path_opt orelse {
+            std.log.err("missing path argument", .{});
+            return error.Explained;
+        };
+
+        const deps = if (build_dep) &project.build_deps else &project.deps;
+        const dep = for (deps.items) |*d| {
+            if (std.mem.eql(u8, d.alias, alias)) break d;
+        } else {
+            const deps_type = if (build_dep) "build dependencies" else "dependencies";
+            std.log.err("Failed to find '{s}' in {s}", .{ alias, deps_type });
+            return error.Explained;
+        };
+
+        const redirect_deps = if (build_dep) &redirects.build_deps else &redirects.deps;
+        for (redirect_deps.items) |d| if (std.mem.eql(u8, d.alias, alias)) {
+            std.log.err("'{s}' is already redirected", .{alias});
+            return error.Explained;
+        };
+
+        try redirect_deps.append(dep.*);
+        const root = switch (dep.src) {
+            .pkg => {
+                // TODO: read gyro.zzz
+                return error.Todo;
+            },
+            .github => |github| github.root,
+            .url => |url| url.root,
+            .local => |local| local.root,
+        };
+
+        dep.* = Dependency{
+            .alias = alias,
+            .src = .{
+                .local = .{
+                    .path = path,
+                    .root = root,
+                },
+            },
+        };
+    }
+
+    try redirects.toFile(redirect_file);
+    try project.toFile(project_file);
+}
