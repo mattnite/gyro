5b98a8fcb9be5401173477604a7de432eb704e7a
Nameless <truemedian@gmail.com>
2021-07-04 22:35:22 -0500

    build: update build.zig for master, reorganize

--- build.zig
+++ build.zig
@@ -1,6 +1,5 @@
 usingnamespace std.build;
 const std = @import("std");
-const ssl = @import(pkgs.ssl.path);
 const pkgs = @import("deps.zig").pkgs;
 
 const clap = .{
@@ -8,20 +7,27 @@ const clap = .{
     .path = .{ .path = "libs/zig-clap/clap.zig" },
 };
 
-const version = .{
-    .name = "version",
-    .path = .{ .path = "libs/version/src/main.zig" },
+const zfetch = .{
+    .name = "zfetch",
+    .path = .{ .path = "libs/zfetch/src/main.zig" },
     .dependencies = &[_]Pkg{
         .{
-            .name = "mecha",
-            .path = .{ .path = "libs/mecha/mecha.zig" },
+            .name = "iguanaTLS",
+            .path = .{ .path = "libs/iguanaTLS/src/main.zig" },
+        },
+        .{
+            .name = "network",
+            .path = .{ .path = "libs/zig-network/network.zig" },
+        },
+        .{
+            .name = "uri",
+            .path = .{ .path = "libs/zig-uri/uri.zig" },
+        },
+        .{
+            .name = "hzzp",
+            .path = .{ .path = "libs/hzzp/src/main.zig" },
         },
     },
-};
-
-const tar = .{
-    .name = "tar",
-    .path = .{ .path = "libs/tar/src/main.zig" },
 };
 
 const zzz = .{
@@ -34,24 +40,18 @@ const glob = .{
     .path = .{ .path = "libs/glob/src/main.zig" },
 };
 
-const hzzp = .{
-    .name = "hzzp",
-    .path = .{ .path = "libs/hzzp/src/main.zig" },
+const tar = .{
+    .name = "tar",
+    .path = .{ .path = "libs/tar/src/main.zig" },
 };
 
-const zfetch = .{
-    .name = "zfetch",
-    .path = .{ .path = "libs/zfetch/src/main.zig" },
-    .dependencies = &[_]std.build.Pkg{
-        hzzp,
-        uri,
+const version = .{
+    .name = "version",
+    .path = .{ .path = "libs/version/src/main.zig" },
+    .dependencies = &[_]Pkg{
         .{
-            .name = "network",
-            .path = .{ .path = "libs/zig-network/network.zig" },
-        },
-        .{
-            .name = "iguanaTLS",
-            .path = .{ .path = "libs/iguanaTLS/src/main.zig" },
+            .name = "mecha",
+            .path = .{ .path = "libs/mecha/mecha.zig" },
         },
     },
 };
@@ -76,34 +76,31 @@ fn addAllPkgs(lib: *LibExeObjStep) void {
     lib.addPackage(uri);
     lib.addPackage(known_folders);
 }
+
 pub fn build(b: *Builder) !void {
-    var target = b.standardTargetOptions(.{});
+    const target = b.standardTargetOptions(.{});
     const mode = b.standardReleaseOptions();
 
-    const bootstrap = b.option(bool, "bootstrap", "bootstrapping with just the zig compiler");
+    const bootstrap = b.option(bool, "bootstrap", "bootstrapping with just the zig compiler") orelse false;
     const repository = b.option([]const u8, "repo", "default package index (default is astrolabe.pm)");
 
     const gyro = b.addExecutable("gyro", "src/main.zig");
     gyro.setTarget(target);
     gyro.setBuildMode(mode);
-    if (bootstrap) |bs| {
-        if (bs) {
-            addAllPkgs(gyro);
-        } else {
-            pkgs.addAllTo(gyro);
-        }
+    gyro.addBuildOption([]const u8, "default_repo", repository orelse "astrolabe.pm");
+    gyro.install();
+
+    if (bootstrap) {
+        addAllPkgs(gyro);
     } else {
         pkgs.addAllTo(gyro);
     }
 
-    gyro.addBuildOption([]const u8, "default_repo", repository orelse "astrolabe.pm");
-    gyro.install();
-
     const tests = b.addTest("src/main.zig");
     tests.setBuildMode(mode);
-    addAllPkgs(tests);
     tests.addBuildOption([]const u8, "default_repo", repository orelse "astrolabe.pm");
-    tests.step.dependOn(b.getInstallStep());
+
+    addAllPkgs(tests);
 
     const test_step = b.step("test", "Run tests");
     test_step.dependOn(&tests.step);
