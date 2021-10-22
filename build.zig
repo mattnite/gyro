57fcb4d4b0111974f0aac47c159340795ec0872a
Matt Knight <mattnite@protonmail.com>
2021-10-22 01:32:02 -0700

    try env var

--- build.zig
+++ build.zig
@@ -102,4 +102,13 @@ pub fn build(b: *Builder) !void {
 
     const test_step = b.step("test", "Run tests");
     test_step.dependOn(&tests.step);
+
+    const run_cmd = gyro.run();
+    run_cmd.step.dependOn(b.getInstallStep());
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
 }
