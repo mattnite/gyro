6f42f61184429196c161c1f1aefd7af496d2f580
Jake Chvatal <jakechvatal@gmail.com>
2022-12-09 18:52:45 -0500

    Add nix flake build (#186)
    * most of build finished? error union issue
    
    * finish flake
    
    * remove gyro from devshell

---
+++ flake.nix
@@ -0,0 +1,79 @@
+{
+  description = "A Zig package manager with an index, build runner, and build dependencies.";
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-22.05;
+    zig.url     = github:mitchellh/zig-overlay;
+    utils.url   = github:numtide/flake-utils;
+
+    # Used for shell.nix
+    flake-compat = {
+      url = github:edolstra/flake-compat;
+      flake = false;
+    };
+  };
+
+  outputs = {self, nixpkgs, zig, utils, ...} @ inputs: with utils.lib;
+    eachSystem allSystems (system: let
+
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [
+          (final: prev: {
+            zigpkgs = inputs.zig.packages.${prev.system};
+            # zig = inputs.zig.packages.${zigVersion}.${prev.system};
+          })
+        ];
+      };
+
+      # Our supported systems are the same supported systems as the Zig binaries
+      systems = builtins.attrNames inputs.zig.packages;
+
+      pname = "gyro";
+      version = "0.7.0";
+
+      zigVersion = "0.10.0";
+      z = pkgs.zigpkgs.${zigVersion};
+
+      gyro = pkgs.stdenv.mkDerivation {
+        inherit pname version;
+        src = ./.;
+        nativeBuildInputs = with pkgs; [ z ];
+        buildInputs = with pkgs; [ ];
+        dontConfigure = true;
+
+        preBuild = ''
+          export HOME=$TMPDIR
+        '';
+
+        installPhase = ''
+          runHook preInstall
+          zig build -Drelease-safe --prefix $out install
+          runHook postInstall
+        '';
+
+        installFlags = ["DESTDIR=$(out)"];
+
+        meta = with pkgs.lib; {
+          description = "A Zig package manager with an index, build runner, and build dependencies.";
+          license = licenses.mit;
+          platforms = platforms.linux;
+          maintainers = with maintainers; [ jakeisnt ];
+        };
+      };
+
+    in rec {
+      packages = {
+        gyro = gyro;
+        default = gyro;
+      };
+
+      defaultPackage = gyro;
+
+      devShells.default = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [ ];
+        buildInputs = with pkgs; [ z ];
+      };
+
+      devShell = self.devShells.${system}.default;
+    });
+}
