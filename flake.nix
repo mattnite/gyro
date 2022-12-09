d4419c5297aed1ebf6f3019a82ba72951cdd9b23
Jacob Chvatal <jake@isnt.online>
2022-12-09 13:43:17 -0500

    finish flake

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "A package manager for the Zig programming language.";
+  description = "A Zig package manager with an index, build runner, and build dependencies.";
   inputs = {
     nixpkgs.url = github:NixOS/nixpkgs/nixos-22.05;
     zig.url     = github:mitchellh/zig-overlay;
@@ -14,11 +14,13 @@
 
   outputs = {self, nixpkgs, zig, utils, ...} @ inputs: with utils.lib;
     eachSystem allSystems (system: let
+
       pkgs = import nixpkgs {
         inherit system;
         overlays = [
           (final: prev: {
             zigpkgs = inputs.zig.packages.${prev.system};
+            # zig = inputs.zig.packages.${zigVersion}.${prev.system};
           })
         ];
       };
@@ -29,35 +31,33 @@
       pname = "gyro";
       version = "0.7.0";
 
+      zigVersion = "0.10.0";
+      z = pkgs.zigpkgs.${zigVersion};
+
       gyro = pkgs.stdenv.mkDerivation {
         inherit pname version;
         src = ./.;
-        nativeBuildInputs = with pkgs; [
-          git
-          mercurial
-          wget
-          unzip
-          gnutar
-          zigpkgs.master
-          pkg-config
-        ];
+        nativeBuildInputs = with pkgs; [ z ];
         buildInputs = with pkgs; [ ];
         dontConfigure = true;
+
         preBuild = ''
           export HOME=$TMPDIR
         '';
 
         installPhase = ''
           runHook preInstall
-          zig build -Drelease-safe
+          zig build -Drelease-safe --prefix $out install
           runHook postInstall
         '';
 
         installFlags = ["DESTDIR=$(out)"];
 
-        meta = {
-          maintainers = [ "Jake Chvatal <jake@isnt.online>" ];
-          description = "gyro";
+        meta = with pkgs.lib; {
+          description = "A Zig package manager with an index, build runner, and build dependencies.";
+          license = licenses.mit;
+          platforms = platforms.linux;
+          maintainers = with maintainers; [ jakeisnt ];
         };
       };
 
@@ -70,17 +70,8 @@
       defaultPackage = gyro;
 
       devShells.default = pkgs.mkShell {
-        nativeBuildInputs = with pkgs; [
-          zigpkgs.master
-        ];
-
-        buildInputs = with pkgs; [
-          git
-          mercurial
-          wget
-          unzip
-          gnutar
-        ];
+        nativeBuildInputs = with pkgs; [ ];
+        buildInputs = with pkgs; [ z gyro ];
       };
 
       devShell = self.devShells.${system}.default;
